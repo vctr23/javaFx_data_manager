5dd20cef48599f80c6f0855763260a61312696a0
Sebastian-Manrique <smanriquemontiel@gmail.com>
2025-02-12 19:22:58 +0100

    Added the function

--- src/main/java/org/example/proyecto_javafx_ficheros/HelloController.java
+++ src/main/java/org/example/proyecto_javafx_ficheros/HelloController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import java.io.File;  // Import the File class
+import java.io.IOException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -32,6 +34,21 @@ public class HelloController implements Initializable {
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void createDocument(MouseEvent event) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
