db214c89084d1a33612977573e11143ed0b0812d
danielluru <danielluru77@gmail.com>
2025-02-13 17:02:40 +0100

    add CSVtoXMLFile

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.proyecto_javafx_ficheros {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml;
 
 
     opens org.example.proyecto_javafx_ficheros to javafx.fxml;
